37ac107bb0712b4356c0e29122008a381752012b
Krur Schak <krurschak@gmail.com>
2021-04-11 02:25:34 +0200

    initial code commit

---
+++ src/DR.Services.Todo/Controllers/DefaultController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DR.Services.Todo.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok("DR Todo Service");
+    }
+}
